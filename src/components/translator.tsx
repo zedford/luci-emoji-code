b73cacf2f8655a329b49805aa30596fb97a9758f
Zack <zedford@gmail.com>
2020-01-22 22:39:28 -0700

    bare bones for app

---
+++ src/components/translator.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Translator: React.FC<any> = () => {
+  return <div>Test</div>;
+};
+
+export default Translator;
---
+++ src/logic/translations.ts
@@ -0,0 +1,3 @@
+export const translate = (message: string) => {
+  return message;
+};
