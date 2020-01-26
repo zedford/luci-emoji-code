a87d4ef6a685159250f59b8e15de664b3fdb2f43
Zack <zedford@gmail.com>
2020-01-25 21:44:49 -0700

    Luci's code. Updates to app

--- src/logic/translations.ts
+++ src/logic/translations.ts
@@ -1,3 +1,81 @@
-export const translate = (message: string) => {
-  return message + ".";
+var emojiDictionary: { [key: string]: string } = {
+  a: "😘",
+  "😘": "a",
+  b: "😀",
+  "😀": "b",
+  c: "😇",
+  "😇": "c",
+  d: "😍",
+  "😍": "d",
+  e: "😒",
+  "😒": "e",
+  f: "😺",
+  "😺": "f",
+  g: "😴",
+  "😴": "g",
+  h: "😾",
+  "😾": "h",
+  i: "😲",
+  "😲": "i",
+  j: "😏",
+  "😏": "j",
+  k: "😛",
+  "😛": "k",
+  l: "😎",
+  "😎": "l",
+  m: "😳",
+  "😳": "m",
+  n: "😨",
+  "😨": "n",
+  o: "😢",
+  "😢": "o",
+  p: "😱",
+  "😱": "p",
+  q: "😣",
+  "😣": "q",
+  r: "😓",
+  "😓": "r",
+  s: "😮",
+  "😮": "s",
+  t: "😚",
+  "😚": "t",
+  u: "🙃",
+  "🙃": "u",
+  v: "😂",
+  "😂": "v",
+  w: "😋",
+  "😋": "w",
+  x: "😉",
+  "😉": "x",
+  y: "😁",
+  "😁": "y",
+  z: "😶",
+  "😶": "z",
+  " ": "😷",
+  "😷": " ",
+  ".": "😵",
+  "😵": ".",
+  ",": "😽",
+  "😽": ",",
+  "?": "😃",
+  "😃": "?",
+  "!": "😝",
+  "😝": "!"
 };
+
+export const translate = (message: string): string => {
+  let characterList = [...message];
+  let translatedCharacterList: string[] = [];
+
+  characterList.forEach(character => {
+    translatedCharacterList.push(emojiDictionary[character]);
+  });
+
+  return translatedCharacterList.join("");
+};
+
+export const translate_daddys = (message: string): string =>
+  [...message].reduce(
+    (translation, current) => translation + (emojiDictionary[current] || ""),
+    ""
+  );
